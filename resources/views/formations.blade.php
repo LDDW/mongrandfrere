99bbc6fbb49c2a5604e3ff83c2dc77fad4e85760
lddw <drouinbenjamin20@gmail.com>
2023-06-10 12:43:02 +0200

    add customer formations page

--- resources/views/formations.blade.php
+++ resources/views/formations.blade.php
@@ -1,3 +1,56 @@
 <x-app-layout>
-    
+    <section>        
+        <div class="w-full bg-white p-4 rounded-xl" data-aos="zoom-in-up">
+            <h1 class="text-[#57C5B6] text-xl uppercase font-bold mb-3">Profiter de notre formation 100% gratuite</h1>
+            <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ipsum voluptates quae aperiam! Laborum eaque quia doloribus perferendis autem obcaecati ullam omnis itaque? Quasi impedit accusamus cumque laudantium, laborum accusantium.</p>
+            <a href="" class="mt-4 block">
+                <button class="py-2.5 px-4 bg-[#57C5B6] text-white rounded-xl">Je profite de la formation gratuite</button>
+            </a>
+        </div>
+    </section>
+    <section>
+        <h2 class="text-2xl font-semibold">Toutes nos formations</h2>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 relative">
+            <div class="absolute w-full h-full -z-10 opacity-20 bg_icon"></div>
+            <a href="" data-aos="zoom-in-up">
+                <div class="bg-white p-4 rounded-xl">
+                    <div class="w-full aspect-square bg-gray-300 rounded-xl overflow-hidden mb-3">
+                        <img src="" alt="">
+                    </div>
+                    <h3 class="text-xl font-semibold truncate mb-2">Formation 1 fzebf hzebfzb efbz nfjz enfjezn fzebf ef zefnzeu fnbze</h3>
+                    <p class="mb-4 truncate">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+                    <div class="flex items-center justify-between">
+                        <button class="py-2.5 px-4 bg-[#57C5B6] text-white rounded-xl">Voir la formation</button>
+                        <p class="truncate shrink-0 text-xl">99.99€</p>
+                    </div>
+                </div>
+            </a>
+            <a href="" data-aos="zoom-in-up" data-aos-delay="100">
+                <div class="bg-white p-4 rounded-xl">
+                    <div class="w-full aspect-square bg-gray-300 rounded-xl overflow-hidden mb-3">
+                        <img src="" alt="">
+                    </div>
+                    <h3 class="text-xl font-semibold truncate mb-2">Formation 1 fzebf hzebfzb efbz nfjz enfjezn fzebf ef zefnzeu fnbze</h3>
+                    <p class="mb-4 truncate">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+                    <div class="flex items-center justify-between">
+                        <button class="py-2.5 px-4 bg-[#57C5B6] text-white rounded-xl">Voir la formation</button>
+                        <p class="truncate shrink-0 text-xl">99.99€</p>
+                    </div>
+                </div>
+            </a>
+            <a href="" data-aos="zoom-in-up" data-aos-delay="200">
+                <div class="bg-white p-4 rounded-xl">
+                    <div class="w-full aspect-square bg-gray-300 rounded-xl overflow-hidden mb-3">
+                        <img src="" alt="">
+                    </div>
+                    <h3 class="text-xl font-semibold truncate mb-2">Formation 1 fzebf hzebfzb efbz nfjz enfjezn fzebf ef zefnzeu fnbze</h3>
+                    <p class="mb-4 truncate">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+                    <div class="flex items-center justify-between">
+                        <button class="py-2.5 px-4 bg-[#57C5B6] text-white rounded-xl">Voir la formation</button>
+                        <p class="truncate shrink-0 text-xl">99.99€</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </section>
 </x-app-layout>
