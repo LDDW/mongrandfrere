6c37733ae0f9142e730314803fd85c2e82ff4f90
LDDW <69431543+LDDW@users.noreply.github.com>
2023-06-14 13:29:47 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
+                'resources/css/app.min.css',
                 'resources/css/glide.core.min.css',
                 'resources/js/app',
                 'resources/js/glide.conf.js',
